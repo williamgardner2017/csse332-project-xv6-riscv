8c14035501ba58ba6f9c8a6b00d2466839e70330
rhit-silkaijr <silkaijr@rose-hulman.edu>
2024-02-13 11:12:36 -0500

    thread.c add

---
+++ kernel/thread.c
@@ -0,0 +1,11 @@
+int create_thread(thread_obj_t* thread, void(*func), void* args) {
+    return 0;
+}
+
+int join_thread(thread_obj_t* thread) {
+    return 0;
+}
+
+int destroy_thread(thread_obj_t* thread) {
+    return 0;
+}
