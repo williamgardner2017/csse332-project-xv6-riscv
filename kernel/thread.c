aee8cbbd1fd74c2615e8c7af0f73f5614bfd5d6a
rhit-silkaijr <silkaijr@rose-hulman.edu>
2024-02-13 11:48:35 -0500

    thread.c changes

--- kernel/thread.c
+++ kernel/thread.c
@@ -1,11 +1,33 @@
-int create_thread(thread_obj_t* thread, void(*func), void* args) {
+#include "proc.c"
+
+int thread_create(thread_obj_t* thread, void(*func)(void*), void* args) {
+    struct proc* newproc;
+    struct proc* currentproc = myproc();
+    if(( newproc = allocproc()) = 0) {
+	return -1;
+    }
+
+    //Copy over heap and globals, do stack magic to start new while keeping reference to old
+    
+    thread->pid = newproc->pid;
+
+    release(&newproc->lock);
+
+    acquire(&wait_lock);
+    newprocp->parent = currentproc;
+    release(&wait_lock);
+
+    acquire(&np->lock);
+    newproc->state = RUNNABLE;
+    release(&np->lock);
+
     return 0;
 }
 
-int join_thread(thread_obj_t* thread) {
+int thread_join(thread_obj_t* thread) {
     return 0;
 }
 
-int destroy_thread(thread_obj_t* thread) {
+int thread_destroy(thread_obj_t* thread) {
     return 0;
 }
