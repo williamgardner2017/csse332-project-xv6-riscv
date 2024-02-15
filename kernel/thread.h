21a90710e9a2442f04e8e7561624d661227a7d17
John William Gardner <william.gardner2017@outlook.com>
2024-02-15 11:09:17 -0500

    thread.h fix

--- kernel/thread.h
+++ kernel/thread.h
@@ -4,6 +4,6 @@ struct thread_obj_t
   void *ret;
 };
 
-int thread_create((struct thread_obj_t) *t, void (*fn)(void *), (void *)arg);
+int thread_create((struct thread_obj_t) *t, void (*fn)(void *), void *arg);
 int thread_join(struct thread_obj_t *thread);
 int thread_destroy(struct thread_obj_t *thread);
