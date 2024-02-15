d52d4cb35bac43b3f7fb44e8ccff366f11cc067b
William Greenlee <greenlee04@gmail.com>
2024-02-15 10:53:40 -0500

    fixes

--- kernel/thread.c
+++ kernel/thread.c
@@ -1,50 +1,56 @@
 #include "proc.c"
 
-int thread_create(thread_obj_t* thread, void(*func)(void*), void* args) {
-    struct proc* newproc;
-    struct proc* currentproc = myproc();
-    if(( newproc = allocproc()) = 0) {
-	return -1;
-    }
+int thread_create(thread_obj_t *thread, void (*fn)(void *), void *args)
+{
+  struct proc *newproc;
+  struct proc *currentproc = myproc();
+  if ((newproc = allocproc()) = 0)
+  {
+    return -1;
+  }
 
-    //Copy over heap and globals, do stack magic to start new while keeping reference to old
-    
-    thread->pid = newproc->pid;
+  // Copy over heap and globals, do stack magic to start new while keeping reference to old
 
-    release(&newproc->lock);
+  thread->pid = newproc->pid;
 
-    acquire(&wait_lock);
-    newprocp->parent = currentproc;
-    release(&wait_lock);
+  release(&newproc->lock);
 
-    acquire(&np->lock);
-    newproc->state = RUNNABLE;
-    release(&np->lock);
+  acquire(&wait_lock);
+  newprocp->parent = currentproc;
+  release(&wait_lock);
 
-    return 0;
+  acquire(&np->lock);
+  newproc->state = RUNNABLE;
+  release(&np->lock);
+
+  return 0;
 }
 
-int thread_join(thread_obj_t* thread) {
-    struct proc* currentproc = myproc();
-    struct proc* childproc = 0; 
+int thread_join(thread_obj_t *thread)
+{
+  struct proc *currentproc = myproc();
+  struct proc *childproc = 0;
 
-    acquire(&wait_lock); 
-    for (;;) { // Change this to something that actually makes sense (like checking all threads)
-      if (p->pid == thread->pid) {
-        childproc = p;
-        break;
-      }
+  acquire(&wait_lock);
+  for (;;)
+  { // Change this to something that actually makes sense (like checking all threads)
+    if (p->pid == thread->pid)
+    {
+      childproc = p;
+      break;
     }
+  }
 
-    while (childproc->state != ZOMBIE) {
-      sleep(childproc, &wait_lock);
+  while (childproc->state != ZOMBIE)
+  {
+    sleep(childproc, &wait_lock);
+  }
+  release(&wait_lock);
 
-    }
-    release(&wait_lock); 
-	
-    return 0; 
+  return 0;
 }
 
-int thread_destroy(thread_obj_t* thread) {
-    return kill(thread->pid);
+int thread_destroy(thread_obj_t *thread)
+{
+  return kill(thread->pid);
 }
--- kernel/thread.h
+++ kernel/thread.h
@@ -1,9 +1,9 @@
-struct thread_obj_t {
-  int pid; 
-  void* ret; 
+struct thread_obj_t
+{
+  int pid;
+  void *ret;
 };
 
-int thread_create(thread_obj_t* thread, (void)(*func)(void), void* args); 
-int thread_join(thread_obj_t* thread); 
-int thread_destroy(thread_obj_t* thread); 
-
+int thread_create(thread_obj_t *thread, void (*fn)(void *), void *args);
+int thread_join(thread_obj_t *thread);
+int thread_destroy(thread_obj_t *thread);
