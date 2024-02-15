fc18963d6d9bf5ad40dac5a096b891632dd8e620
rhit-silkaijr <silkaijr@rose-hulman.edu>
2024-02-15 11:25:09 -0500

    test update

--- user/threadtest.c
+++ user/threadtest.c
@@ -5,5 +5,16 @@
 
 int main(int argc, char* argv[]) {
     printf("TESTING\n");
+    struct thread_obj_t* testThread = malloc(sizeof(struct thread_obj_t*);
+    thread_create(testThread, test_fn, NULL);
+    sleep(5);
+    printf("killing thread now");
+    thread_destroy(testThread);
+
     exit(0);
+}
+
+void* test_fn(void* args) {
+    printf("eyyy i'm functioning here\n");
+
 }
