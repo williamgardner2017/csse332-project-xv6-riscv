d96e4d2d427b963fc95f33c0111218250a47816b
rhit-silkaijr <silkaijr@rose-hulman.edu>
2024-02-16 18:28:29 -0500

    changing test

--- user/threadtest.c
+++ user/threadtest.c
@@ -1,24 +1,28 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "kernel/thread.h"
 #include "user/user.h"
 
 void *test_fn(void *args)
 {
     printf("eyyy i'm functioning here\n");
     sleep(10);
+    printf("time to return baybee\n");
     return (void *)5;
+}
+
+void *test_fn_2(void *args) {
+    printf("we loopin here\n");
+    for(;;)
+    return -1;
 }
 
 int main(int argc, char* argv[]) {
     printf("TESTING\n");
-    struct thread_obj_t* testThread = malloc(sizeof(struct thread_obj_t*));
-    printf("trying to create\n");
-    thread_create(testThread, test_fn, 0);
+    int pid = thread_create(test_fn_2, 0);
     printf("create worked\n");
     sleep(5);
     printf("killing thread now\n");
-    thread_destroy(testThread);
+    thread_destroy(pid);
     if(testThread->pid == 0) {
 	printf("properly destroyed\n");
     }
@@ -26,15 +30,9 @@ int main(int argc, char* argv[]) {
 	printf("Did not destroy properly\n");
     }
     printf("Creating a new thread\n");
-    thread_create(testThread, test_fn, 0);
-    thread_join(testThread);
-    if(*(int*) testThread->ret == 5) {
-	printf("We joined right\n");
-    }
-    else {
-	printf("We joined wrong\n");
-    }
-    free(testThread);
+    pid = thread_create(test_fn, 0);
+    thread_join(pid);
+    printf("joined the 2nd thread\n");
     exit(0);
 }
 
